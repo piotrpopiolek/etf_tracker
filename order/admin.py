5cdce1f8f5e32ec6061b899d6d4bf591d1f5e652
Piotr Popiołek <piotr.popiolek93@gmail.com>
2024-04-25 18:42:31 +0200

    Views

--- order/admin.py
+++ order/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Summary, Sum
+
+class SummaryAdmin(admin.ModelAdmin):
+    list_display = ('date_order', 'name', 'amount', 'sum', 'average')
+    list_filter = ['name']
+    ordering = ['date_order', 'amount', 'sum', 'average']
+
+class SumAdmin(admin.ModelAdmin):
+    list_display = ('date_order', 'sum')
 
 admin.site.register(Order)
+admin.site.register(Summary, SummaryAdmin)
+admin.site.register(Sum, SumAdmin)
--- order/models.py
+++ order/models.py
@@ -13,3 +13,28 @@ class Order(models.Model):
     class Meta:
         managed = False
         db_table = 'order'
+
+class Summary(models.Model):
+    date_order = models.DateField(primary_key=True)
+    name = models.CharField(max_length=50)
+    amount = models.FloatField()
+    sum = models.FloatField()
+    average = models.FloatField()
+
+    def __str__(self):
+        return "{} {} {} {} {}".format(self.date_order, self.name, self.amount, self.sum, self.average)
+
+    class Meta:
+        managed = False
+        db_table = 'Summary'
+
+
+class Sum(models.Model):
+    date_order = models.DateField(primary_key=True)
+    sum = models.FloatField()
+    def __str__(self):
+        return "{} {}".format(self.date_order, self.sum)
+
+    class Meta:
+        managed = False
+        db_table = 'Sum'
